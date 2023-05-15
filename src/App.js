784a0ef264e6fc71717f6202286752221588dc7b
kimjinsil <skyisrrna@naver.com>
2023-05-15 14:44:12 +0900

    whatever

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import { GlobalStyles } from "./styles";
 import Header from "./Header";
 import MainImage from "./MainImage";
-
+import Profile from "./Profile";
 
 
 function App() {
   return (
     <>
     {/* css 리셋*/}
- 
+    <GlobalStyles/>
 
     {/*메뉴*/}
     <Header/>
 
     {/* 메인이미지 */}
     <MainImage/>
+
+    {/* 프로파일*/}
+    <Profile />
     </>
   );
 }
